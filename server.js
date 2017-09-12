28064d91c06c751c819dccdc009a3138a3221a0c
Animesh Sinha <animeshsinha19@ccs.neu.edu>
2017-09-11 21:44:23 -0400

    ip and port changed

--- server.js
+++ server.js
@@ -3,8 +3,8 @@ var app = express();
 
 app.use(express.static(__dirname + '/public'));
 
-var ipaddress = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
-var port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
+var ipaddress = 'localhost';
+var port = 5000;
 
 
 app.listen(port, ipaddress);
